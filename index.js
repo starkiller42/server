b635a7c40be47cc814b17cfd56f3061f8cb2d280
Lucas dos Santos Soares <luc_ssoares@outlook.com>
2021-01-20 22:10:29 -0300

    adicionando o crud

--- index.js
+++ index.js
@@ -1,9 +1,58 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const app = express();
+const mysql = require('mysql');
 
-app.get('/', (req, res) => {
-    res.send('Hello Lucas!')
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'luca3715',
+    database: 'CRUDDatabase',
 })
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+// select
+app.get('/api/get', (req, res) => {
+    const sqlSelect = "SELECT * FROM movie_reviews";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result)
+    })
+})
+
+// insert
+app.post('/api/insert', (req, res) => {
+    const movieName = req.body.movieName
+    const movieReview = req.body.movieReview
+
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        res.send(result)
+    })
+})
+
+// delete
+app.delete('/api/delete/:movieName', (req, res) => {
+    const name = req.params.movieName
+    const sqlDelete = "DELETE FROM movie_reviews WHERE movieName = ?";
+    db.query(sqlDelete, name, (err, result) => {
+        if(err) console.log(err)
+    })
+})
+
+// update
+app.put('/api/update', (req, res) => {
+    const name = req.body.movieName
+    const review = req.body.movieReview
+    const sqlUpdate = "UPDATE movie_reviews SET movieReview = ? WHERE movieName = ?";
+    db.query(sqlUpdate, [review ,name], (err, result) => {
+        if(err) console.log(err)
+    })
+})
+
 
 app.listen(3001, () => {
     console.log("rodando na porta 3001");
